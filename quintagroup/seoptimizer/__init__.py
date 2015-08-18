b9de6c3bc89403aba25f26276b4b2450f6a353eb
Maurits van Rees <maurits@vanrees.org>
2015-08-18 15:18:09 +0200

    Do not allow unrestricted access to our util.py.
    It seems this used to be needed by some skin templates that are no longer there
    to access some utility functions that are no longer there.

--- quintagroup/seoptimizer/__init__.py
+++ quintagroup/seoptimizer/__init__.py
@@ -1,6 +1,3 @@
-from AccessControl import allow_module
 from zope.i18nmessageid import MessageFactory
 
 SeoptimizerMessageFactory = MessageFactory('quintagroup.seoptimizer')
-
-allow_module('quintagroup.seoptimizer.util')
